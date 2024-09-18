0466bcf9470e4b2876c3cf3cd6b342404c1b6388
Kaushikidum29 <pallavipradhan5578@gmail.com>
2024-09-18 23:41:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scaffold Example'),
+      ),
+      body: Center(
+        child: Text('Hello, Scaffold!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your action here
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
